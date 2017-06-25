902f74362f7b3392ac46af4243047a53bf9d728b
Kieran <kibu1995@gmail.com>
2017-06-25 08:35:30 +0100

    Basic foundations for the bot

---
+++ main.js
@@ -0,0 +1,26 @@
+﻿const Discord = require("discord.js");
+const Config = require("./config.json");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Logged in and ready');
+});
+
+client.on('message', msg => {
+
+    if (msg.author.bot) return;
+
+
+    switch (msg.content) {
+        case "ping":
+            msg.reply("Pong!");
+            break;
+        default:
+            msg.reply("Not a function");
+            break;
+    }
+
+
+});
+
+client.login(Config.token);
