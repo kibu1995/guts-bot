a7d43457a8068326096ae2b9e65f6c429c236bed
Kieran <kibu1995@gmail.com>
2017-07-09 04:55:34 +0100

    Changes to make the commands modular. Main loads in all files inside commands folder.

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
 ﻿const Discord = require("discord.js");
 const Config = require("./config.json");
 const client = new Discord.Client();
+const utils = require("./utils/utils.js");
+var commands = {};
 
-client.on('ready', () => {
+function loadCommands() {
+    let normalizedPath = require("path").join(__dirname, "commands");
+    let commandList = {};
+
+    require("fs").readdirSync(normalizedPath).forEach(function (file) {
+        let fileName = file.substring(0, file.indexOf('.'));
+        commandList[fileName] = require("./commands/" + file);
+    });
+
+    return commandList;
+}
+
+client.on('ready', function() {
+    commands = loadCommands();
     console.log('Logged in and ready');
-    client.setGame("(╯°□°)╯︵ ┻━┻");
+    client.user.setGame("(╯°□°)╯︵ ┻━┻");
 });
 
-client.on('message', msg => {
+client.on('message', function(msg) {
 
     if (msg.author.bot) return;
 
-    switch (msg.content) {
-        case "ping":
-            msg.reply("Pong!");
-            break;
-        default:
-            msg.reply("Not a function");
-            break;
+    if (msg.content === "help") {
+        utils.help(commands, msg);
+    } else {
+        utils.runCommand(commands, msg);
     }
-
 });
 
-client.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', function(member) {
     let channel = member.guild.channels.find('name', 'general');
     if (!channel) return;
     channel.send(`Welcome to the server, ${member}`);
---
+++ utils/utils.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+    runCommand: function (commands, msg) {
+        if (commands[msg.content]) {
+            // Check if user can use command (is admin / has role)
+            commands[msg.content].run(msg);
+        } else {
+            msg.reply("Not a function");
+        }
+    },
+    help: function (commands, msg) {
+        let helpMessage = "";
+
+        Object.keys(commands).forEach(function (key) {
+            helpMessage += '\n';
+            helpMessage += key + " : " + commands[key].description;
+        });
+
+        msg.reply(helpMessage);
+    }
+}
