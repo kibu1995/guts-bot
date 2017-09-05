36b9de572762f21d7aac615a57f829b85531bfdd
Kieran Burrell <kibu1995@gmail.com>
2017-09-05 04:33:10 +0100

    Added kick function.

---
+++ commands/kick.js
@@ -0,0 +1,18 @@
+﻿module.exports = {
+  "description": "Kicks a given user",
+  "run": function (msg, args) {
+    if (!msg.member.roles.some(r=>["Admin", "Mod"].includes(r.name))) {
+      return msg.reply("Need correct roles to use this");
+    }
+    
+    let member = msg.mentions.members.first();
+    if (!member) return msg.reply("Mention a valid user!");
+    if (!member.kickable) return msg.reply("Can't kick mentioned user!");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason given!";
+    
+    member.kick(reason).catch(error => msg.reply("Can't kick user: " + error));
+    msg.reply("Kicked " + member.user.tag + " for " + reason);
+  }
+}
